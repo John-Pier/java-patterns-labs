ed6784687990cf3b4c6d74a4c49761242ee1e18b
John Pier <warhamer10tw@gmail.com>
2022-05-03 19:31:53 +0400

    lab 3.6 upd

--- lab-3/lab-3-6-state/src/main/java/com/johnpier/lab36state/StateController.java
+++ lab-3/lab-3-6-state/src/main/java/com/johnpier/lab36state/StateController.java
@@ -2,13 +2,30 @@ package com.johnpier.lab36state;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.*;
 
 public class StateController {
-    @FXML
-    private Label welcomeText;
+
+    private final Image semesterImage = new Image("D:\\Projects\\Repos\\java-patterns-labs\\lab-3\\lab-3-6-state\\target\\classes\\com\\johnpier\\lab36state\\img\\sem.png");
+    private final Image vacationImage = new Image("D:\\Projects\\Repos\\java-patterns-labs\\lab-3\\lab-3-6-state\\target\\classes\\com\\johnpier\\lab36state\\img\\vac.png");
+    private final Image sessionImage = new Image("D:\\Projects\\Repos\\java-patterns-labs\\lab-3\\lab-3-6-state\\src\\main\\resources\\com\\johnpier\\lab36state\\img\\ses.png");
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected ImageView imageView;
+
+
+    @FXML
+    protected void onSemesterButtonClick() {
+        imageView.setImage(semesterImage);
+    }
+
+    @FXML
+    protected void onVacationButtonClick() {
+        imageView.setImage(vacationImage);
+    }
+
+    @FXML
+    protected void onSessionButtonClick() {
+        imageView.setImage(sessionImage);
     }
 }
